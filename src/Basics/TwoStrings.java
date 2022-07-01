c26ffe864c881d2bbe60f07a503d72a7cffaebf6
dhanrajpandagale <dhanrajpandagale@gmail.com>
2022-07-01 05:38:45 -0400

    check two strings are equal or not

---
+++ src/Basics/TwoStrings.java
@@ -0,0 +1,24 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class TwoStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1, str2;
+        Scanner scanner = new Scanner(System.in);
+ 
+        System.out.println("Enter first String");
+        str1 = scanner.nextLine();
+ 
+        System.out.println("Enter second String");
+        str2 = scanner.nextLine();
+        // Comparing two input string
+        if (str1.equals(str2))
+            System.out.print("Equal Strings");
+        else
+            System.out.print("UnEqual Strings");
+	}
+
+}
